5230795db12e47b16a68d5ce0912fe2090eb2ad2
James Harris <stoffle@gmail.com>
2012-07-22 20:30:47 +1000

    Added TCON and STATUS support.

--- mcp4xxx.cpp
+++ mcp4xxx.cpp
@@ -3,14 +3,15 @@
 namespace icecave {
 namespace arduino {
 
-MCP4XXX::MCP4XXX(byte select_pin, Pot pot, MemorySize memory_size, WiperConfiguration config)
+MCP4XXX::MCP4XXX(byte select_pin, Pot pot, Resolution resolution, WiperConfiguration config)
   : m_select_pin(select_pin)
-  , m_pot_address(static_cast<Address>(pot))
-  , m_max_value(memory_size + config) // Potentiometer configurations allow "memory_size + 1" values, for setting the "full-scale" wiper position.
+  , m_pot(pot)
+  , m_max_value(resolution + config) // Potentiometer configurations allow "resolution + 1" values, for setting the "full-scale" wiper position.
+  , m_select_nesting(0)
 {
   SPI.begin();
   pinMode(m_select_pin, OUTPUT);
-  deselect();
+  digitalWrite(m_select_pin, HIGH);
 }
 
 
@@ -21,22 +22,22 @@ word MCP4XXX::max_value(void) const
 
 bool MCP4XXX::increment(void)
 {
-  return transfer(m_pot_address, command_increment);
+  return transfer(static_cast<Address>(m_pot), command_increment);
 }
 
 bool MCP4XXX::decrement(void)
 {
-  return transfer(m_pot_address, command_decrement);
+  return transfer(static_cast<Address>(m_pot), command_decrement);
 }
 
 bool MCP4XXX::set(word value)
 {
-  return transfer(m_pot_address, command_write, min(value, m_max_value));
+  return transfer(static_cast<Address>(m_pot), command_write, min(value, m_max_value));
 }
 
 bool MCP4XXX::get(word& value) const
 {
-  return transfer(m_pot_address, command_read, data_mask_word, value);
+  return transfer(static_cast<Address>(m_pot), command_read, data_mask_word, value);
 }
 
 word MCP4XXX::get(void) const
@@ -46,17 +47,115 @@ word MCP4XXX::get(void) const
   return result;
 }
 
+bool MCP4XXX::set_terminal_a_status(bool connected)
+{
+  return set_tcon(tcon_term_a_mask, connected);
+}
+
+bool MCP4XXX::get_terminal_a_status(bool& connected) const
+{
+  return get_tcon(tcon_term_a_mask, connected);
+}
+
+bool MCP4XXX::get_terminal_a_status(void) const
+{
+  bool connected = false;
+  return get_terminal_a_status(connected)
+      && connected;
+}
+
+bool MCP4XXX::set_terminal_b_status(bool connected)
+{
+  return set_tcon(tcon_term_b_mask, connected);
+}
+
+bool MCP4XXX::get_terminal_b_status(bool& connected) const
+{
+  return get_tcon(tcon_term_b_mask, connected);
+}
+
+bool MCP4XXX::get_terminal_b_status(void) const
+{
+  bool connected = false;
+  return get_terminal_b_status(connected)
+      && connected;
+}
+
+bool MCP4XXX::set_wiper_status(bool connected)
+{
+  return set_tcon(tcon_wiper_mask, connected);
+}
+
+bool MCP4XXX::get_wiper_status(bool& connected) const
+{
+  return get_tcon(tcon_wiper_mask, connected);
+}
+
+bool MCP4XXX::get_wiper_status(void) const
+{
+  bool connected = false;
+  return get_wiper_status(connected)
+      && connected;
+}
+
+bool MCP4XXX::set_shutdown_status(bool shutdown)
+{
+  return set_tcon(tcon_shutdown_mask, !shutdown);
+}
+
+bool MCP4XXX::get_shutdown_status(bool& shutdown) const
+{
+  bool enabled;
+  if (get_tcon(tcon_shutdown_mask, enabled))
+  {
+    shutdown = !enabled;
+    return true;
+  }
+  return false;
+}
+
+bool MCP4XXX::get_shutdown_status(void) const
+{
+  bool shutdown = false;
+  return get_shutdown_status(shutdown)
+      && shutdown;
+}
+
+bool MCP4XXX::get_hardware_shutdown_status(bool& shutdown) const
+{
+  word result;
+  if (transfer(address_status, command_read, data_mask_word, result))
+  {
+    shutdown = result & status_shutdown_mask;
+    return true;
+  }
+  return false;
+}
+
+bool MCP4XXX::get_hardware_shutdown_status(void) const
+{
+  bool shutdown = false;
+  return get_hardware_shutdown_status(shutdown)
+      && shutdown;
+}
+
 void MCP4XXX::select(void) const
 {
-  SPI.setBitOrder(MSBFIRST);
-  SPI.setDataMode(SPI_MODE0);
-  SPI.setClockDivider(SPI_CLOCK_DIV128);
-  digitalWrite(m_select_pin, LOW);
+  if (++m_select_nesting == 1)
+  {
+    SPI.setBitOrder(MSBFIRST);
+    SPI.setDataMode(SPI_MODE0);
+    SPI.setClockDivider(SPI_CLOCK_DIV128);
+    digitalWrite(m_select_pin, LOW);
+  }
 }
 
 void MCP4XXX::deselect(void) const
 {
-  digitalWrite(m_select_pin, HIGH);
+  if (--m_select_nesting == 0)
+  {
+    digitalWrite(m_select_pin, HIGH);
+  }
 }
 
 byte MCP4XXX::build_command(Address address, Command command) const
@@ -95,7 +194,7 @@ bool MCP4XXX::transfer(Address address, Command command, word data) const
   deselect();
   return valid;
 }
-    
+
 bool MCP4XXX::transfer(Address address, Command command, word data, word& result) const
 {
   select();
@@ -114,6 +213,64 @@ bool MCP4XXX::transfer(Address address, Command command, word data, word& result
   return valid;
 }
 
+bool MCP4XXX::set_tcon(byte value)
+{
+  // Note that the 9th (reserved) bit is always set to 1.
+  return transfer(address_tcon, command_write, 0x100 | value);
+}
+
+bool MCP4XXX::set_tcon(byte mask, bool value)
+{
+  select();
+
+  // The values for pot #1 are 4 bits higher in the TCON register.
+  if (m_pot == pot_1)
+    mask <<= 4;
+
+  byte tcon;
+  bool success = get_tcon(tcon);
+
+  if (success)
+  {
+    if (value)
+    {
+      success = set_tcon(tcon | mask);
+    }
+    else
+    {
+      success = set_tcon(tcon & ~mask);
+    }
+  }
+
+  deselect();
+  return success;
+}
+
+bool MCP4XXX::get_tcon(byte& value) const
+{
+  word result;
+  if (transfer(address_tcon, command_read, data_mask_word, result))
+  {
+    value = lowByte(result);
+    return true;
+  }
+  return false;
+}
+
+bool MCP4XXX::get_tcon(byte mask, bool& value) const
+{
+  byte tcon;
+  if (get_tcon(tcon))
+  {
+    // The values for pot #1 are 4 bits higher in the TCON register.
+    if (m_pot == pot_1)
+      mask <<= 4;
+
+    value = tcon & mask;
+    return true;
+  }
+  return false;
+}
+
 } // end namespace arduino
 } // end namespace icecave
-
--- mcp4xxx.h
+++ mcp4xxx.h
@@ -17,12 +17,12 @@ namespace arduino {
  *       | | |
  *       | | +- Z = Wiper Configuration (1 = Potentiometer, 2 = Rheostat).
  *       | |
- *       | +--- Y = Memory Type (3 = 7-bit RAM, 4 = 7-bit EEPROM, 5 = 8-bit RAM, 6 = 8-bit EEPROM).
+ *       | +--- Y = Memory Type / Resolution (3 = 7-bit RAM, 4 = 7-bit EEPROM, 5 = 8-bit RAM, 6 = 8-bit EEPROM).
  *       |
  *       +----- X = Number of pots (1 or 2).
  *
  * Note that the MCP41X1 chips multiplex the SDI and SDO on a single pin.
- * To use these chips with a standard SPI interface as on the Arduino you will need to 
+ * To use these chips with a standard SPI interface as on the Arduino you will need to
  * connect the SDI/SDO pin on the pot to the Arduino's MISO pin, then bridge the MISO pin to the MOSI pin
  * with a resistor (3k9 resistor seems to work well).
  *
@@ -31,35 +31,39 @@ namespace arduino {
 class MCP4XXX
 {
   public:
+
+    // Which pot to use for dual devices (MCP42XX).
     enum Pot
     {
-      pot_0 = B00,
-      pot_1 = B01
+        pot_0 = B00
+      , pot_1 = B01
     };
-    
-    enum MemorySize
+
+    // The resolution of the device's resistor ladder.
+    enum Resolution
     {
-      memory_7bit = 127,
-      memory_8bit = 255
+        res_7bit = 127
+      , res_8bit = 255
     };
-    
+
+    // Whether the device is a rheostat (MCP4XX2), or potentiometer (MCP4XX1).
     enum WiperConfiguration
     {
-      rheostat = 0,
-      potentiometer = 1
+        rheostat = 0
+      , potentiometer = 1
     };
 
     /**
      * select_pin  - The pin to use for the SPI slave select signal, defaults to the main slave select pin on your Arduino.
      * pot         - For the 2-pot variants (MCP42XX), the potentiometer to control. Must be pot_0 for MCP41XX chips.
-     * memory_size - memory_7bit for MCP4X3X and MCP4X4X, memory_8bit for MCP4X5X and MCP4X6X.
+     * resolution  - res_7bit for MCP4X3X and MCP4X4X, res_8bit for MCP4X5X and MCP4X6X.
      */
-    MCP4XXX(byte select_pin = SS, Pot pot = pot_0, MemorySize memory_size = memory_8bit, WiperConfiguration config = potentiometer);
-    
+    MCP4XXX(byte select_pin = SS, Pot pot = pot_0, Resolution resolution = res_8bit, WiperConfiguration config = potentiometer);
+
     /**
      * Retrieve the maximum value allowed for the wiper position.
      *
-     * The maximum value depends on the device's memory type and wiper configuration.
+     * The maximum value depends on the device's resolution and wiper configuration.
      *
      * 7-bit devices have a maximum value of 127 for rheostats and 128 for potentiometers.
      * 8-bit devices have a maximum value of 255 for rheostats and 256 for potentiometers.
@@ -108,12 +112,150 @@ class MCP4XXX
      */
     word get(void) const;
 
+    /**
+     * Set the status of terminal "A".
+     *
+     * This method will always fail on rheostat (MCP4XX2) devices.
+     * Use set_terminal_b_status() instead.
+     *
+     * connected - TRUE to connect, FALSE to disconnect.
+     *
+     * Returns TRUE on success; otherwise, FALSE.
+     */
+    bool set_terminal_a_status(bool connected);
+
+    /**
+     * Get the status of terminal "A".
+     *
+     * This method will always fail on rheostat (MCP4XX2) devices.
+     * Use get_terminal_b_status() instead.
+     *
+     * connected - Assigned TRUE if connected, FALSE if disconnected.
+     *
+     * Returns TRUE on success; otherwise FALSE.
+     */
+    bool get_terminal_a_status(bool& connected) const;
+
+    /**
+     * Get the status of terminal "A".
+     *
+     * This method will always fail on rheostat (MCP4XX2) devices.
+     * Use get_terminal_b_status() instead.
+     *
+     * Returns TRUE if the command is successful AND the terminal is connected; otherwise FALSE.
+     */
+    bool get_terminal_a_status(void) const;
+
+    /**
+     * Set the status of terminal "B".
+     *
+     * connected - TRUE to connect, FALSE to disconnect.
+     *
+     * Returns TRUE on success; otherwise, FALSE.
+     */
+    bool set_terminal_b_status(bool connected);
+
+    /**
+     * Get the status of terminal "B".
+     *
+     * connected - Assigned TRUE if connected, FALSE if disconnected.
+     *
+     * Returns TRUE on success; otherwise FALSE.
+     */
+    bool get_terminal_b_status(bool& connected) const;
+
+    /**
+     * Get the status of terminal "B".
+     *
+     * Returns TRUE if the command is successful AND the terminal is connected; otherwise FALSE.
+     */
+    bool get_terminal_b_status(void) const;
+
+    /**
+     * Set the status of the wiper.
+     *
+     * connected - TRUE to connect, FALSE to disconnect.
+     *
+     * Returns TRUE on success; otherwise, FALSE.
+     */
+    bool set_wiper_status(bool connected);
+
+    /**
+     * Get the status of the wiper.
+     *
+     * connected - Assigned TRUE if connected, FALSE if disconnected.
+     *
+     * Returns TRUE on success; otherwise FALSE.
+     */
+    bool get_wiper_status(bool& connected) const;
+
+    /**
+     * Get the status of the wiper.
+     *
+     * Returns TRUE if the command is successful AND the wiper is connected; otherwise FALSE.
+     */
+    bool get_wiper_status(void) const;
+
+    /**
+     * Set the software shutdown status.
+     *
+     * shutdown - TRUE to shutdown, FALSE to enable.
+     *
+     * Returns TRUE on success; otherwise, FALSE.
+     */
+    bool set_shutdown_status(bool shutdown);
+
+    /**
+     * Get the software shutdown status.
+     *
+     * Note that although the shutdown behaviour is overridden by the hardware SHDN pin (if present),
+     * this method will still return the software setting. To get the hardware status use get_hardware_shutdown_status().
+     *
+     * shutdown - Assigned TRUE if shutdown, FALSE if enabled.
+     *
+     * Returns TRUE on success; otherwise, FALSE.
+     */
+    bool get_shutdown_status(bool& shutdown) const;
+
+    /**
+     * Get the software shutdown status.
+     *
+     * Note that although the shutdown behaviour is overridden by the hardware SHDN pin (if present),
+     * this method will still return the software setting. To get the hardware status use get_hardware_shutdown_status().
+     *
+     * Returns TRUE if the command is successful AND the device is in a software shutdown state.
+     */
+    bool get_shutdown_status(void) const;
+
+    /**
+    * Get the status of the hardware shutdown bin, if present.
+     *
+     * shutdown - Assigned TRUE if shutdown, FALSE if enabled.
+     *
+     * Returns TRUE on success; otherwise FALSE.
+     */
+    bool get_hardware_shutdown_status(bool& shutdown) const;
+
+    /**
+     * Get the status of the hardware shutdown bin, if present.
+     *
+     * Returns TRUE if the command is successful AND the device is shutdown; otherwise FALSE.
+     */
+    bool get_hardware_shutdown_status(void) const;
+
   private:
-    const static byte address_mask       = B11110000;
-    const static byte command_mask       = B00001100;
-    const static byte cmderr_mask        = B00000010;
-    const static byte data_mask          = B00000001;
-    const static word data_mask_word     = 0x01FF;
+    const static byte address_mask   = B11110000;
+    const static byte command_mask   = B00001100;
+    const static byte cmderr_mask    = B00000010;
+    const static byte data_mask      = B00000001;
+    const static word data_mask_word = 0x01FF;
+
+    const static byte tcon_shutdown_mask = B1000;
+    const static byte tcon_term_a_mask   = B0100;
+    const static byte tcon_term_b_mask   = B0010;
+    const static byte tcon_wiper_mask    = B0001;
+
+    const static word status_shutdown_mask = B10;
 
     enum Address
     {
@@ -122,7 +264,7 @@ class MCP4XXX
       , address_tcon       = B0100
       , address_status     = B0101
     };
-    
+
     enum Command
     {
         command_write      = B00
@@ -157,16 +299,16 @@ class MCP4XXX
 
     /**
      * Transfer an 8-bit command.
-     */    
+     */
     bool transfer(Address address, Command command) const;
-    
+
     /**
      * Transfer a 16-bit command.
      *
      * Result is TRUE if address/command combination is valid; otherwise, FALSE.
      */
     bool transfer(Address address, Command command, word data) const;
-    
+
     /**
      * Transfer a 16-bit command, and read the response.
      *
@@ -176,9 +318,50 @@ class MCP4XXX
      */
     bool transfer(Address address, Command command, word data, word& result) const;
 
+    /**
+     * Set the value of the TCON register.
+     *
+     * Returns TRUE on success; otherwise, FALSE.
+     */
+    bool set_tcon(byte value);
+
+    /**
+     * Set the value of a bit within the TCON register.
+     *
+     * mask - One of the TCON bit masks.
+     * value - The value of the TCON bit for the given mask.
+     *
+     * Note that the mask is automatically shifted to the correct position based on m_pot.
+     *
+     * Returns TRUE on success; otherwise, FALSE.
+     */
+    bool set_tcon(byte mask, bool value);
+
+    /**
+     * Get the value of the TCON register.
+     *
+     * value - Assigned the value of the TCON register.
+     *
+     * Returns TRUE on success; otherwise, FALSE.
+     */
+    bool get_tcon(byte& value) const;
+
+    /**
+     * Get the value of a bit within the TCON register.
+     *
+     * mask - One of the TCON bit masks.
+     * value - Assigned the value of the TCON bit for the given mask.
+     *
+     * Note that the mask is automatically shifted to the correct position based on m_pot.
+     *
+     * Returns TRUE on success; otherwise, FALSE.
+     */
+    bool get_tcon(byte mask, bool& value) const;
+
     byte m_select_pin;
-    Address m_pot_address;
+    Pot m_pot;
     word m_max_value;
+    mutable unsigned int m_select_nesting;
 };
 
 } // end namespace arduino
